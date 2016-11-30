c9a6f108812693487f73f941ea8806703484d681
German Escobar <german.escobarc@gmail.com>
2016-11-30 12:50:03 -0500

    Prueba actualizada y modificada

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+      super()
+
+    this.state = {
+      isChecked:false
+
+
+    };
+  }
+  handleCheckboxChange (event) {
+
+       this.setState({isChecked: event.target.checked});
+   }
+
+
+
   render() {
+ const paragraph = <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>;
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="wrapper">
+        <label><input type="checkbox" onChange={this.handleCheckboxChange.bind(this)} checked={this.state.isChecked} /> Mostrar información importante</label>
+        {this.state.isChecked ? paragraph : null}
       </div>
     );
   }
